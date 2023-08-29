aa4ca26785f144adaeb0261eec5a6121b07fd65b
Theo Chatonnet <theo.chatonnet@gmail.com>
2023-08-29 12:59:47 +0200

    link between index-show-edit

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,8 +1,7 @@
 class InvoicesController < ApplicationController
   # before_action :authenticate_user!
   def index
-    @invoices = Invoice.all.order(emission_date: :desc)
-    @user = User.find(params[:user_id])
+    @invoices = current_user.invoices.order(emission_date: :desc)
     if params[:query].present?
       sql_subquery = "number ILIKE :query "
       @invoices = @invoices.where(sql_subquery, query: "%#{params[:query]}%")
@@ -11,6 +10,7 @@ class InvoicesController < ApplicationController
 
   def show
     @invoice = Invoice.find(params[:id])
+    @user = User.find(params[:user_id])
   end
 
   def new
@@ -28,24 +28,24 @@ class InvoicesController < ApplicationController
     end
   end
 
-    def edit
-      @invoice = Invoice.find(params[:id])
-      @user = User.find(params[:user_id])
-    end
+  # def edit
+  #   @invoice = Invoice.find(params[:id])
+  #   @user = User.find(params[:user_id])
+  # end
 
-    def update
-      @invoice = Invoice.find(params[:id])
-      @invoice.update(invoice_params)
-      if @invoice.save
-        redirect_to user_invoices_path
-      else
-        render :edit, status: :unprocessable_entity
-      end
+  def update
+    @invoice = Invoice.find(params[:id])
+    @invoice.update(invoice_params)
+    if @invoice.save
+      redirect_to user_invoices_path
+    else
+      render :edit, status: :unprocessable_entity
     end
+  end
 
   private
 
   def invoice_params
-    params.require(:invoice).permit(:number, :amount, :emission_date, :payment_date, :comment, :progress, :user_id, :debtor_id)
+    params.require(:invoice).permit(:number, :amount, :emission_date, :payment_date, :comment, :progress, :debtor_id)
   end
 end
