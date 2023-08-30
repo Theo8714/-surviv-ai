0a0de2c61bd8139707ac1d9fd18833101c6d6e13
Maxime Oudin <oudinmaxime@gmail.com>
2023-08-30 14:32:39 +0200

    filterallcards to be done in js controller progress search

--- app/javascript/controllers/progress_search_controller.js
+++ app/javascript/controllers/progress_search_controller.js
@@ -3,42 +3,63 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["card"]
 
+  filter = []
+
   connect() {
-    this.filters = []
   }
 
   searchTraiter() {
-    // ajouter le statut cliqué dans l'array this.filter ou l'enlever si présent
-    // jouer la methode filterAllCards
-    this.filterCards("a-traiter");
+    if (this.filter.includes("a-traiter")) {
+      const index = this.filter.indexOf("a-traiter");
+      if (index !== -1) {
+        this.filter.splice(index, 1);
+      }
+    } else {
+      this.filter.push("a-traiter");
+    }
+    this.filterAllCards("a-traiter")
   }
 
   searchEcheance() {
-    this.filterCards("avant-echeance");
+    if (this.filter.includes("avant-echeance")) {
+      const index = this.filter.indexOf("avant-echeance");
+      if (index !== -1) {
+        this.filter.splice(index, 1);
+      }
+    } else {
+      this.filter.push("avant-echeance");
+    }
+    this.filterAllCards("avant-echeance")
   }
 
   searchAmiable() {
-    this.filterCards("phase-amiable");
+    if (this.filter.includes("phase-amiable")) {
+      const index = this.filter.indexOf("phase-amiable");
+      if (index !== -1) {
+        this.filter.splice(index, 1);
+      }
+    } else {
+      this.filter.push("phase-amiable");
+    }
+    this.filterAllCards("phase-amiable")
   }
 
   searchJuridique() {
-    this.filterCards("juridique");
+    if (this.filter.includes("juridique")) {
+      const index = this.filter.indexOf("juridique");
+      if (index !== -1) {
+        this.filter.splice(index, 1);
+      }
+    } else {
+      this.filter.push("juridique");
+    }
+    this.filterAllCards("juridique")
   }
 
-  filterAllCards() {
-    // forEach card
-    // vérifier si cardStatus === un élément de l'array this.filter
-    // si c'est le cas class remove d.none
-    // sinon add d-none
-  }
-
-  filterCards(status) {
+  filterAllCards(status) {
     this.cardTargets.forEach((card) => {
       const cardStatus = card.dataset.status;
-      console.log(cardStatus === status || status === "Tous");
-      console.log(cardStatus);
-      console.log(status);
-      if (cardStatus === status ) {
+      if (this.filter.includes(status)) {
         card.classList.remove("d-none");
       } else {
         card.classList.add("d-none");
