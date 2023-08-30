3c9eced99e976731aace617171a7b9c9cd657f67
Theo Chatonnet <theo.chatonnet@gmail.com>
2023-08-30 17:07:03 +0200

    Affichage Dashboard

--- app/services/debtor_analyzer.rb
+++ app/services/debtor_analyzer.rb
@@ -1,21 +1,25 @@
 class DebtorAnalyzer
+  attr_reader :debtor, :average, :unpaid_amount
+
   def initialize(relationship)
     @debtor = relationship.debtor
     @user = relationship.user
     @invoices = relationship.invoices
   end
 
-  # def perform
-  #   average_invoice_amount
-  #   total_unpaid_amount
-  # end
-
-  def average_invoice_amount
-    total_amount = @invoices.sum(:amount)
-    @average = invoices.count.positive? ? total_amount / invoices.count : 0
+  def perform
+    calculate_average_invoice_amount
+    calculate_total_unpaid_amount
   end
 
-  def total_unpaid_amount
+  private
+
+  def calculate_average_invoice_amount
+    total_amount = @invoices.sum(:amount)
+    @average = @invoices.count.positive? ? total_amount / @invoices.count : 0
+  end
+
+  def calculate_total_unpaid_amount
     unpaid_invoices = @invoices.where(progress: "Phase amiable")
     @unpaid_amount = unpaid_invoices.sum(:amount)
   end
