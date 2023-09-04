f49382c0f857f198e459b3ac2a5534ccba70cbf1
Pablo Preault <pablo.preault@kedgebs.com>
2023-09-04 14:12:50 +0200

    modif done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :new_invoice
 
-  def new_invoice
-    @invoice_new = Invoice.new
-  end
 end
