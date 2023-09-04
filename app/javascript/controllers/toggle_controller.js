ade35f7dd2934f5f3620207824c191d0115108a4
Pablo Preault <pablo.preault@kedgebs.com>
2023-09-04 17:11:23 +0200

    added a lot of things

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -6,9 +6,13 @@ export default class extends Controller {
   connect() {
     console.log("Hello from toggle_controller.js");
   }
-  static targets = ["togglableElement"]
+  static targets = ["togglableElement", "togglableElem", "toggableClass", "toggableCl"]
 
   fire() {
     this.togglableElementTarget.classList.toggle("d-none");
+    this.togglableElemTarget.classList.toggle("d-none");
+    this.toggableClassTarget.classList.toggle("clicked");
+    this.toggableClTarget.classList.toggle("clicked");
   }
+
 };
