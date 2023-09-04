3f0891568ff2767d6b93c884889eca8d3f7de0de
Pablo Preault <pablo.preault@kedgebs.com>
2023-09-04 12:48:36 +0200

    adding invoices is now a canva , some fix on the search bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :new_invoice
+
+  def new_invoice
+    @invoice_new = Invoice.new
+  end
 end
